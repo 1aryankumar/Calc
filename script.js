3a87223e017dec405536ea170bb1b294506c6625
Aryan Kumar <yankumar101@gmail.com>
2023-10-07 17:55:50 +0530

    V3

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const display = document.querySelector('.display'); 
 const buttons = document.querySelectorAll('button'); 
 
+const calulate = document.querySelector
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => calulate(e.target.dataset.value)); });
