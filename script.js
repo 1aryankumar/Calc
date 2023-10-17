c2923e964c64481ff6f3d64eadc3cb841c4260e6
Aryan Kumar <yankumar101@gmail.com>
2023-10-17 23:06:56 +0530

    V4

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const display = document.querySelector('.display'); 
-const buttons = document.querySelectorAll('button'); 
-const specialChar = ['+', '-', '*', '÷',"%","="];
-const calulate = document.querySelector
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => calulate(e.target.dataset.value)); });
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", handleClick);
+});
+
+let expression = "";
+
+function handleClick(e) {
+    const buttonValue = e.target.getAttribute("data-value");
+
+    switch (buttonValue) {
+        case "=":
+            try {
+           
+                const result = eval(expression);
+                display.value = result;
+                expression = result;
+            } catch (error) {
+                display.value = "Error";
+                expression = "";
+            }
+            break;
+        case "AC":
+        
+            display.value = "";
+            expression = "";
+            break;
+        case "DEL":
+        
+            expression = expression.slice(0, -1);
+            display.value = expression;
+            break;
+        default:
+         
+            expression += buttonValue;
+            display.value = expression;
+    }
+}
